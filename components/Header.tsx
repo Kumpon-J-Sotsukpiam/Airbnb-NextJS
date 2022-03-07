3b321a88ff68f8860d9f37a307ea66d8882ec53f
kumpon J Sotsukpiam <kumpon_s@brainergy.digital>
2022-03-08 06:03:26 +0700

    update: the header is done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import Image from 'next/image'
-import { SearchIcon } from '@heroicons/react/solid';
+import {
+    GlobeAltIcon,
+    MenuIcon,
+    UserCircleIcon,
+    UsersIcon,
+    SearchIcon
+} from '@heroicons/react/solid';
 
 export default class Header extends Component {
     render() {
@@ -31,7 +37,16 @@ export default class Header extends Component {
                         h-8 text-white rounded-full bg-red-400 p-2 cursor-pointer' />
                 </div>
                 {/* Right */}
-                <div></div>
+                <div className='flex
+                        items-center justify-end space-x-4 text-gray-500'
+                >
+                    <p className='hidden md:inline-flex cursor-pointer'>Become a host</p>
+                    <GlobeAltIcon className='h-6 cursor-pointer' />
+                    <div className='flex items-center space-x-2 border-2 p-2 rounded-full '>
+                        <MenuIcon className='h-6' />
+                        <UserCircleIcon className='h-6' />
+                    </div>
+                </div>
             </header>
         )
     }
