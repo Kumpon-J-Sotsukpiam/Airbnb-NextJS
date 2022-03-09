260f02d2205576d9b89e2728451dd2817fca0ac3
kumpon J Sotsukpiam <kumpon_s@brainergy.digital>
2022-03-10 02:18:37 +0700

    update: feature search

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -11,7 +11,7 @@ export default class Banner extends Component {
                     objectFit='cover'
                 />
                 <div className='absolute top-1/2 w-full text-center'>
-                    <p className='text-sm: text-lg'>Not sure where to go? Perfact.</p>
+                    <p className='text-sm: text-lg'>Not sure where to go? Perfect.</p>
                     <button
                         className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 
                         hover:shadow-xl active:scale-90 transition duration-150'>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,53 +1,101 @@
-import React, { Component } from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import {
     GlobeAltIcon,
     MenuIcon,
     UserCircleIcon,
     UsersIcon,
-    SearchIcon
+    SearchIcon,
+    UserIcon
 } from '@heroicons/react/solid';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker, RangeKeyDict } from 'react-date-range';
 
-export default class Header extends Component {
-    render() {
-        return (
-            <header
-                className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10'
-            >
-                {/* Left */}
-                <div className='relative flex items-center h-10 cursor-pointer my-auto'>
-                    <Image
-                        src='https://links.papareact.com/qd3'
-                        layout='fill'
-                        objectFit='contain'
-                        objectPosition='left'
-                    />
-                </div>
-                {/* Middle -Search */}
-                <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-                    <input
-                        type="text"
-                        placeholder='Start your search'
-                        className='flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400' />
-                    <SearchIcon
-                        className='
+export default function Header() {
+    const [searchInput, setSearchInput] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [numOfGuests, setNumOfGuests] = useState(1);
+
+    const selectionRange = {
+        startDate,
+        endDate,
+        key: 'selection'
+    }
+    const handleSelect = (ranges: RangeKeyDict | any) => {
+        console.log(ranges);
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+    const resetInput = () => {
+        setSearchInput('');
+    }
+    return (
+        <header
+            className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10'
+        >
+            {/* Left */}
+            <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+                <Image
+                    src='https://links.papareact.com/qd3'
+                    layout='fill'
+                    objectFit='contain'
+                    objectPosition='left'
+                />
+            </div>
+            {/* Middle -Search */}
+            <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+                <input
+                    type="text"
+                    placeholder='Start your search'
+                    className='flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400'
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                />
+                <SearchIcon
+                    className='
                         hidden
                         md:inline-flex
                         md: mx-2
                         h-8 text-white rounded-full bg-red-400 p-2 cursor-pointer' />
-                </div>
-                {/* Right */}
-                <div className='flex
+            </div>
+            {/* Right */}
+            <div className='flex
                         items-center justify-end space-x-4 text-gray-500'
-                >
-                    <p className='hidden md:inline-flex cursor-pointer'>Become a host</p>
-                    <GlobeAltIcon className='h-6 cursor-pointer' />
-                    <div className='flex items-center space-x-2 border-2 p-2 rounded-full '>
-                        <MenuIcon className='h-6' />
-                        <UserCircleIcon className='h-6' />
+            >
+                <p className='hidden md:inline-flex cursor-pointer'>Become a host</p>
+                <GlobeAltIcon className='h-6 cursor-pointer' />
+                <div className='flex items-center space-x-2 border-2 p-2 rounded-full '>
+                    <MenuIcon className='h-6' />
+                    <UserCircleIcon className='h-6' />
+                </div>
+            </div>
+            {searchInput && (
+                <div className='flex flex-col col-span-3 mx-auto'>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={['#FD5B61']}
+                        onChange={handleSelect}
+                    />
+                    <div className='flex items-center border-b mb-4'>
+                        <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+                        <UserIcon className='h-5' />
+                        <input
+                            className='w-12 pl-2 text-lg outline-none text-red-400'
+                            type="number"
+                            value={numOfGuests}
+                            onChange={e => setNumOfGuests(Number(e.target.value))}
+                            min={1}
+                        />
+                    </div>
+                    <div className='flex'>
+                        <button className='flex-grow text-gray-500' onClick={resetInput}>Cancel</button>
+                        <button className='flex-grow text-red-400'>Search</button>
                     </div>
                 </div>
-            </header>
-        )
-    }
+            )}
+        </header>
+    )
 }
